be75709eed03756dac0be82250f3857b437c56f8
Sergey <32326767+laver0ck@users.noreply.github.com>
2019-04-18 18:43:44 +0300

    Update react-theme.js

--- react-theme.js
+++ react-theme.js
@@ -1,22 +1,24 @@
-// in variables.css
+// src/utils/variables.css or smth
 
-// default theme
+// first - common stuff 
 :root {
     --font-default-size: 16px;
     --font-size-small: 14px;
     --font-size-big: 18px;
     ...
-    /* this part is default dark theme */
+    // second - things to be changed by theme
     --background-color: rgb(35, 44, 49);
     --font-color-default: rgb(255, 255, 255);
     --font-color-gray: rgb(155, 155, 155);
 }
 
-:root[data-theme="light"] {
+// third - alternative theme with same vars as in second
+:root[data-theme="light"] { 
     --background-color: rgb(255, 255, 255);
     --font-color-default: rgb(0, 0, 0);
     --font-color-gray: rgb(74, 74, 74);
 }
+// Don't forget to `@import "styles/variables.css";` in index.css
 
 
 // ---------------------------
